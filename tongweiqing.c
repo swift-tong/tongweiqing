82cecc7514c7f1089d448c86963ddb1c2d471f6a
swift-tong <tongweiqing1015@163.com>
2016-03-28 17:56:35 +0800

    Create tongweiqing.c

---
+++ tongweiqing.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+  char*name = "tongweiqing";
+  int age = 25;
+  char* company = "Pactera";
+  char* love = "haiyang";
+  printf("name:%s\n",name);
+  printf("name:%s\n",name);
+}
